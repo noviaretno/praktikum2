e7746afc94a2c31a028dab13879d2f32c1728223
noviaretno <noviaretno>
2018-04-02 22:57:45 +0800

    membuat atribut dan Behavior

--- src/praktikum2/pembelianhelm.java
+++ src/praktikum2/pembelianhelm.java
@@ -1,5 +1,14 @@
 
 package praktikum2;
 public class pembelianhelm {
+    String nama;
+    int hrgasatuan;
+    int jmlhpembelian;
+    
+    void cetakInfo(){
+        System.out.println("nama         : "+nama);
+        System.out.println("harga satuan : "+hrgasatuan);
+        System.out.println("jumlah pembelian : "+jmlhpembelian);
+    }
     
 }
