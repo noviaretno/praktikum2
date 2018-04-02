a5fa5f8bf8e8ceb38d47073b1a9fff6e04563b33
noviaretno <noviaretno>
2018-04-02 23:15:02 +0800

    mengisi nilai atribut

--- src/praktikum2/pembelianhelmaksi.java
+++ src/praktikum2/pembelianhelmaksi.java
@@ -2,7 +2,12 @@
 package praktikum2;
 public class pembelianhelmaksi {
     public static void main(String[] args) {
-       pembelianhelm helm1 = new pembelianhelm();
-       
+        pembelianhelm h1 = new pembelianhelm();
+        h1.nama="GM";
+        h1.hrgasatuan=300000;
+        h1.jmlhpembelian=2;
+        
+        
+        
     }
 }
