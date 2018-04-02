aa102edf41f8951e611d83e86bc9837c6ee63fe4
noviaretno <noviaretno>
2018-04-02 23:47:54 +0800

    constructor dengan parameter

--- src/praktikum2/pembelianhelm.java
+++ src/praktikum2/pembelianhelm.java
@@ -4,6 +4,19 @@ public class pembelianhelm {
     String nama;
     int hrgasatuan;
     int jmlhpembelian;
+
+    public pembelianhelm() {
+        nama="NHK";
+        hrgasatuan=500000;
+        jmlhpembelian=3;
+    }
+
+    public pembelianhelm(String nama, int hrgasatuan, int jmlhpembelian) {
+        this.nama = nama;
+        this.hrgasatuan = hrgasatuan;
+        this.jmlhpembelian = jmlhpembelian;
+    }
+    
     
     void cetakInfo(){
         System.out.println("nama         : "+nama);
--- src/praktikum2/pembelianhelmaksi.java
+++ src/praktikum2/pembelianhelmaksi.java
@@ -11,6 +11,10 @@ public class pembelianhelmaksi {
         System.out.println("total pembelian"+h1.totalpembelian());
         h1.cetaktotal();
         
+        pembelianhelm h2 = new pembelianhelm();
+        h2.cetakInfo();
+        
+        pembelianhelm h3 = new pembelianhelm(1500000,3);
         
     }
 }
